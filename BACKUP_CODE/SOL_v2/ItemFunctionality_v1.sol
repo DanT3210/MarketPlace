f54ae7de90f4d2bfc33383337c12df49cd33b377
Dant3210 <daneel1982@gmail.com>
2022-10-08 00:36:52 -0400

    renamed:    contracts/PublishFunctionalities.sol -> BACKUP_CODE/SOL_v1/PublishFunctionalities_110Test.sol 	renamed:    contracts/PublishOracle.sol -> BACKUP_CODE/SOL_v1/PublishOracle_110Test.sol 	renamed:    test/PublishV2_Test.js -> BACKUP_CODE/SOL_v1/PublishV2_Test.js 	renamed:    BACKUP_CODE/SOL_v2/ItemFunctionality.sol -> BACKUP_CODE/SOL_v2/ItemFunctionality_v1.sol 	renamed:    BACKUP_CODE/SOL_v2/item.sol -> BACKUP_CODE/SOL_v2/item_v1.sol 	new file:   contracts/ItemFunctionality.sol 	new file:   contracts/item.sol 	new file:   test/itemV1_Test.js

---
+++ BACKUP_CODE/SOL_v2/ItemFunctionality_v1.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.15;
+//@Title: Online Trade Marker (blockchain eBay)
+//@author: Dant3210
+//@notice: Functions Contract
+//@dev: Contract under development (work in progress )
+//@version: remix
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract ItemFunctionality is Ownable, ReentrancyGuard{ 
+
+
+    event TransferSingle(address operator, address from,address to,uint256 id,uint256 amount);
+    event TransferBatch(address operator, address from, address to,uint256[] ids,uint256[] amounts);
+    event ApprovalForAll(address owner, address operator, bool approved);
+
+    // Mapping from token ID to account balances *Item QTY Own by an account*
+    mapping(uint256 => mapping(address => uint256)) private _itemQTY;
+
+    // Mapping from account to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    // Used as the URI for all token types by relying on ID substitution, e.g. https://token-cdn-domain/{id}.json
+    string private _uri;
+
+    constructor() {
+    }
+
+   
+    function uri(uint256) public view virtual returns (string memory) {
+        return _uri;
+    }
+
+    function item_qty(address account, uint256 id) public view virtual returns (uint256) {
+        require(account != address(0), "ERROR: address zero is not a valid owner");
+        return _itemQTY[id][account];
+    }
+
+    function item_qtyOfBatch(address[] memory accounts, uint256[] memory ids)public view virtual returns (uint256[] memory){
+        require(accounts.length == ids.length, "ERROR: accounts and ids length mismatch");
+
+        uint256[] memory batchBalances = new uint256[](accounts.length);
+
+        for (uint256 i = 0; i < accounts.length; ++i) {
+            batchBalances[i] = item_qty(accounts[i], ids[i]);
+        }
+
+        return batchBalances;
+    }
+
+
+    function setApprovalForAll(address operator, bool approved) internal virtual {
+        _setApprovalForAll(_msgSender(), operator, approved);
+    }
+
+    function isApprovedForAll(address account, address operator) internal view virtual returns (bool) {
+        return _operatorApprovals[account][operator];
+    }
+
+    function safeTransferFrom(address from, address to, uint256 id, uint256 amount) internal virtual {
+        require(from == _msgSender() || isApprovedForAll(from, _msgSender()),"ERROR: caller is not token owner or approved");
+        _safeTransferFrom(from, to, id, amount);
+    }
+
+    function safeBatchTransferFrom(address from, address to,uint256[] memory ids, uint256[] memory amounts) internal virtual{
+        require(from == _msgSender() || isApprovedForAll(from, _msgSender()), "ERROR: caller is not token owner or approved");
+        _safeBatchTransferFrom(from, to, ids, amounts);
+    }
+
+    function _safeTransferFrom(address from, address to, uint256 id, uint256 amount) internal virtual {
+        require(to != address(0), "ERROR: transfer to the zero address");
+
+        address operator = _msgSender();
+        //uint256[] memory ids = _asSingletonArray(id);
+        //uint256[] memory amounts = _asSingletonArray(amount);
+
+        uint256 fromBalance = _itemQTY[id][from];
+        require(fromBalance >= amount, "ERROR: insufficient balance for transfer");
+        unchecked {
+            _itemQTY[id][from] = fromBalance - amount;
+        }
+        _itemQTY[id][to] += amount;
+
+        emit TransferSingle(operator, from, to, id, amount);
+    }
+
+    function _safeBatchTransferFrom(address from, address to, uint256[] memory ids, uint256[] memory amounts) internal virtual {
+        require(ids.length == amounts.length, "ERROR: ids and amounts length mismatch");
+        require(to != address(0), "ERROR: transfer to the zero address");
+
+        address operator = _msgSender();
+
+        for (uint256 i = 0; i < ids.length; ++i) {
+            uint256 id = ids[i];
+            uint256 amount = amounts[i];
+
+            uint256 fromBalance = _itemQTY[id][from];
+            require(fromBalance >= amount, "ERROR: insufficient QTY for transfer");
+            unchecked {
+                _itemQTY[id][from] = fromBalance - amount;
+            }
+            _itemQTY[id][to] += amount;
+        }
+
+        emit TransferBatch(operator, from, to, ids, amounts);
+    }
+
+    function _setURI(string memory newuri) internal virtual onlyOwner{
+        _uri = newuri;
+    }
+
+    //PUBLISH SINGLE ITEM//
+    function _mint(address to, uint256 id, uint256 amount) internal virtual {
+        require(to != address(0), "ERROR: mint to the zero address");
+
+        address operator = _msgSender();
+        //uint256[] memory ids = _asSingletonArray(id);
+        //uint256[] memory amounts = _asSingletonArray(amount);
+
+        _itemQTY[id][to] += amount;
+        emit TransferSingle(operator, address(0), to, id, amount);
+    }
+
+    //PUBLIS BATCH ITEMS//
+    function _mintBatch( address to,uint256[] memory ids, uint256[] memory amounts) internal virtual {
+        require(to != address(0), "ERROR: mint to the zero address");
+        require(ids.length == amounts.length, "ERROR: ids and amounts length mismatch");
+
+        address operator = _msgSender();
+
+        for (uint256 i = 0; i < ids.length; i++) {
+            _itemQTY[ids[i]][to] += amounts[i];
+        }
+
+        emit TransferBatch(operator, address(0), to, ids, amounts);
+    }
+
+    //REMOVED SINGLE ITEM//
+    function _burn(address from, uint256 id, uint256 amount) internal virtual {
+        require(from != address(0), "ERROR: burn from the zero address");
+
+        address operator = _msgSender();
+        //uint256[] memory ids = _asSingletonArray(id);
+        //uint256[] memory amounts = _asSingletonArray(amount);
+
+        uint256 fromBalance = _itemQTY[id][from];
+        require(fromBalance >= amount, "ERROR: burn amount exceeds balance");
+        unchecked {
+            _itemQTY[id][from] = fromBalance - amount;
+        }
+
+        emit TransferSingle(operator, from, address(0), id, amount);
+    }
+
+    //REMOVE BATCH ITEMS//
+    function _burnBatch(address from, uint256[] memory ids, uint256[] memory amounts) internal virtual {
+        require(from != address(0), "ERROR: burn from the zero address");
+        require(ids.length == amounts.length, "ERROR: ids and amounts length mismatch");
+
+        address operator = _msgSender();
+
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint256 id = ids[i];
+            uint256 amount = amounts[i];
+
+            uint256 fromBalance = _itemQTY[id][from];
+            require(fromBalance >= amount, "ERROR: burn amount exceeds balance");
+            unchecked {
+                _itemQTY[id][from] = fromBalance - amount;
+            }
+        }
+
+        emit TransferBatch(operator, from, address(0), ids, amounts);
+    }
+
+    function _setApprovalForAll(address owner, address operator, bool approved) internal virtual {
+        require(owner != operator, "ERROR: setting approval status for self");
+        _operatorApprovals[owner][operator] = approved;
+        emit ApprovalForAll(owner, operator, approved);
+    }
+}
---
+++ test/itemV1_Test.js
@@ -0,0 +1,164 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+let price, owner, addr1, addr2, addr3, hardhatMarket, itemStatus;
+
+describe("Market contract Test", function () {
+  it("Deployment", async function () {
+    [owner, addr1, addr2, addr3] = await ethers.getSigners();
+    //parameters
+    const brand="Adiddas";
+    const model="Yeezy";
+    const description="Zebra";
+    price="9000000"
+    const size="9.5";
+  
+    const MarketCont = await ethers.getContractFactory("PublishFunctionalities");
+
+    hardhatMarket = await MarketCont.deploy();
+
+    const hardhardPublis= await hardhatMarket._AddItem(brand,model,description,price,size);
+   //const arrayVariable=await hardhatMarket.sellerItemList("0",owner.address);
+   itemStatus= await hardhatMarket._itemStatus("0", owner.address);
+   console.log("STATUS______________________");
+   console.log(itemStatus);
+   //console.log("Listed");
+  });
+
+/*
+  it("Price Update", async function(){
+    price=100;
+    const fundReleased= await hardhatMarket.connect(owner)._updatePrice("0",price);
+    //itemStatus= await hardhatMarket._itemStatus("0", owner.address);
+    console.log("Price Updated______________________");
+
+  });
+  
+  it("Buy Item", async function(){
+    const buyItem=await hardhatMarket.connect(addr1)._buyItem(owner.address, "0", { value: price });
+    console.log("_____________________________________________________________________________");
+    itemStatus= await hardhatMarket._itemStatus("0", owner.address);
+    console.log(itemStatus);
+    console.log("END BUY-----------------");
+  });  
+
+  it("Cancel Order", async function(){
+    const CancelItem= await hardhatMarket.connect(addr1)._cancelTransaction(owner.address,"0", "DEMO");
+    itemStatus= await hardhatMarket._itemStatus("0", owner.address);
+    console.log(itemStatus);
+    console.log("END Cancel-----------------");
+  });
+
+  it("Buyer Withdraw Funds", async function(){
+    const BuyerWithdrawFunds= await hardhatMarket.connect(addr1)._realeaseFunds("0");
+    itemStatus= await hardhatMarket._itemStatus("0", owner.address);
+    console.log(itemStatus);    
+    console.log("BUYER WITHDRAW--------------------");
+  });
+
+  it("Relist item", async function(){
+    const activate= await hardhatMarket.connect(owner)._relistItem("0");
+    itemStatus= await hardhatMarket._itemStatus("0", owner.address);
+    console.log(itemStatus);
+    console.log("ITEM RELISTED--------------------");
+  });
+
+  it("Check Item Listed", async function(){
+    const itemListed_var=await hardhatMarket.itemListedBySeller("0", owner.address);
+    itemStatus= await hardhatMarket._itemStatus("0", owner.address);
+    //console.log(itemListed_var);
+    console.log(itemStatus);
+    console.log("SHOW ITEM LISTED-----------------");
+  }); 
+
+  it("Delete item", async function(){
+    const removeItem= await hardhatMarket.connect(owner)._unPublish("0");
+    const itemListed_var=await hardhatMarket.itemListedBySeller("0", owner.address);
+    itemStatus= await hardhatMarket._itemStatus("0", owner.address);
+    console.log(itemStatus);
+    console.log("ITEM REMOVED--------------------");
+    console.log(itemListed_var);
+  });  
+
+  it("New Listed", async function(){
+    const brand="Nike";
+    const model="AJ1";
+    const description="First Class";
+    price="19000000"
+    const size="9";
+
+   const hardhardPublis= await hardhatMarket._AddItem(brand,model,description,price,size);
+   const arrayVariable=await hardhatMarket.itemListedBySeller("1",owner.address);
+   itemStatus= await hardhatMarket._itemStatus("1", owner.address);
+   console.log(itemStatus);
+   console.log("STATUS______________________");
+   console.log(arrayVariable);
+  });  
+
+  it("Buy Item", async function(){
+    const buyItem=await hardhatMarket.connect(addr2)._buyItem(owner.address, "1", { value: price });
+    console.log("Buy Add2_______________________________________________________________________");
+    itemStatus= await hardhatMarket._itemStatus("1", owner.address);
+    console.log(itemStatus);
+    console.log("END BUY 2-----------------");
+  });  
+
+  it("Ship Item", async function(){
+    //owner=addr2;
+    console.log("------------SHIPPED-------------------------------------------");
+    const shipped= await hardhatMarket.connect(owner)._shipping("12345566","1");
+    itemStatus= await hardhatMarket._itemStatus("1", owner.address);
+    console.log(itemStatus);
+    console.log("END SHIP-----------------");
+  });  
+
+  it("Received Item", async function(){
+    const receivedItem= await hardhatMarket.connect(addr2)._receivedItem(owner.address,"1","All OK");
+    itemStatus= await hardhatMarket._itemStatus("1", owner.address);
+    console.log(itemStatus);
+    console.log("END RECEIVED-----------------");
+  });
+
+  it("Seller Amount", async function(){
+    const balanceOffItem= await hardhatMarket.connect(addr2).ballanceOff("1",owner.address);
+    console.log(balanceOffItem.toString());
+    console.log("SELLER BALANCEOFF-----------------");
+  });
+  
+  it("Return Item", async function(){
+    const ReturnItem=await hardhatMarket.connect(addr2)._returnItem(owner.address,"1", "12222111212");
+    itemStatus= await hardhatMarket._itemStatus("1", owner.address);
+    console.log(itemStatus);
+    console.log("END RETURN-----------------");
+  });
+
+  it("Seller Received Item", async function(){
+    const receivedItem2= await hardhatMarket.receivedReturn("1","All OK");
+    itemStatus= await hardhatMarket._itemStatus("1", owner.address);
+    console.log(itemStatus);
+    console.log("END SELLER RECEIVED-----------------");
+  });  
+
+  it("Buyer Amount", async function(){
+    const balanceOffItem= await hardhatMarket.ballanceOff("1",addr2.address);
+    console.log(balanceOffItem.toString());
+    console.log("BUYER BALANCEOFF-----------------");
+    const balanceOffSeller= await hardhatMarket.ballanceOff("1",owner.address);
+    console.log(balanceOffSeller.toString());    
+    console.log("SELLER BALANCEOFF-----------------");
+  });
+
+  ////////*******Need to wait few seconds (Fund the way to simulate time in test*********
+  it("Buyer Withdraw Funds", async function(){
+    const BuyerWithdrawFunds= await hardhatMarket.connect(addr2)._realeaseFunds("1");
+    itemStatus= await hardhatMarket._itemStatus("1", owner.address);
+    console.log(itemStatus);    
+    console.log("BUYER2 WITHDRAW--------------------");
+  });
+
+  /*it("Remove Item", async function(){
+    const removedItem= await hardhatMarket._unPublish("0");
+    console.log(removedItem);
+  });*/
+  
+});
